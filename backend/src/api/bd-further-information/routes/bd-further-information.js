a34c2431e73c59a05bc332ac33f337dafd7303f4
teske00 <ntesovc27@gmail.com>
2025-03-30 10:36:39 +0200

    fix: Fix bd controller

--- backend/src/api/bd-further-information/routes/bd-further-information.js
+++ backend/src/api/bd-further-information/routes/bd-further-information.js
@@ -1,28 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  * bd-further-information router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::bd-further-information.bd-further-information', {
+module.exports = createCoreRouter(
+  "api::bd-further-information.bd-further-information",
+  {
     config: {
       find: {
-        roles: ["authenticated"],
+        roles: ["authenticated", "public"],
       },
       create: {
         roles: ["authenticated"],
       },
-      findOne: { 
-        roles: ["authenticated"],
+      findOne: {
+        roles: ["authenticated", "public"],
       },
       update: {
         roles: ["authenticated"],
-      }, 
+      },
       delete: {
         roles: ["authenticated"],
       },
     },
-  });
-
+  }
+);
