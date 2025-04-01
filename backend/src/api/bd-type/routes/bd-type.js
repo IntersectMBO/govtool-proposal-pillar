b63d78e246339a7ac643b873fa2dc3dd76edf416
teske00 <ntesovc27@gmail.com>
2025-04-01 16:34:51 +0200

    fix: Fix api permisions

--- backend/src/api/bd-type/routes/bd-type.js
+++ backend/src/api/bd-type/routes/bd-type.js
@@ -1,27 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * bd-type router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::bd-type.bd-type', {
-    config: {
-      find: {
-        roles: ["authenticated","public"],
-      },
-      create: {
-        roles: ["authenticated","public"],
-      },
-      findOne: {
-        roles: ["authenticated","public"],
-      },
-      update: {
-        roles: ["authenticated","public"],
-      },
-      delete: {
-        roles: ["authenticated","public"],
-      },
+module.exports = createCoreRouter("api::bd-type.bd-type", {
+  config: {
+    find: {
+      roles: ["authenticated", "public"],
     },
-  });
+    findOne: {
+      roles: ["authenticated", "public"],
+    },
+  },
+});
