756cc8940a81e4a9d186687a76ac52bdc0044752
Vukasin Paunovic <vukasinpaunovich@gmail.com>
2025-04-04 13:05:18 +0200

    fix: handle duplicates

--- backend/src/api/bd/controllers/bd.js
+++ backend/src/api/bd/controllers/bd.js
@@ -6,266 +6,325 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::bd.bd", ({ strapi }) => ({
-  async create(ctx) {
-    try {
-      const user = ctx.state.user;
-      if (!user) return ctx.unauthorized("Unknown user");
+async function getLastOldVersion(oldVerId) {
+	let currentId = oldVerId;
+	let chainContinues = true;
+	while (chainContinues) {
+		const entries = await strapi.entityService.findMany('api::bd.bd', {
+			filters: {
+				old_ver: currentId,
+			},
+			populate: ['old_ver'],
+		});
+		if (entries?.length > 0) {
+			const entry = entries[0];
+			if (entry && entry?.id) {
+				currentId = entry?.id;
+			} else {
+				chainContinues = false;
+			}
+		} else {
+			chainContinues = false;
+		}
+	}
+	return currentId;
+}
 
-      const { data } = ctx.request.body;
-      if (!data.privacy_policy) {
-        return ctx.badRequest("Privacy policy must be accepted");
-      }
+module.exports = createCoreController('api::bd.bd', ({ strapi }) => ({
+	async create(ctx) {
+		try {
+			const user = ctx.state.user;
+			if (!user) return ctx.unauthorized('Unknown user');
 
-      const savedEntities = {};
+			const { data } = ctx.request.body;
+			if (!data.privacy_policy) {
+				return ctx.badRequest('Privacy policy must be accepted');
+			}
 
-      // BD-PSAPB
-      if (data.bd_psapb) {
-        savedEntities.bd_psapb = await strapi.entityService.create(
-          "api::bd-psapb.bd-psapb",
-          {
-            data: data.bd_psapb,
-          }
-        );
-      }
+			const savedEntities = {};
 
-      // BD-Costing
-      if (data.bd_costing) {
-        savedEntities.bd_costing = await strapi.entityService.create(
-          "api::bd-costing.bd-costing",
-          {
-            data: data.bd_costing,
-          }
-        );
-      }
+			// BD-PSAPB
+			if (data.bd_psapb) {
+				savedEntities.bd_psapb = await strapi.entityService.create(
+					'api::bd-psapb.bd-psapb',
+					{
+						data: data.bd_psapb,
+					}
+				);
+			}
 
-      // BD-Proposal-Details
-      if (data.bd_proposal_detail) {
-        savedEntities.bd_proposal_detail = await strapi.entityService.create(
-          "api::bd-proposal-detail.bd-proposal-detail",
-          {
-            data: data.bd_proposal_detail,
-          }
-        );
-      }
+			// BD-Costing
+			if (data.bd_costing) {
+				savedEntities.bd_costing = await strapi.entityService.create(
+					'api::bd-costing.bd-costing',
+					{
+						data: data.bd_costing,
+					}
+				);
+			}
 
-      // BD-Proposal-Ownership
-      if (data.bd_proposal_ownership) {
-        savedEntities.bd_proposal_ownership = await strapi.entityService.create(
-          "api::bd-proposal-ownership.bd-proposal-ownership",
-          {
-            data: data.bd_proposal_ownership,
-          }
-        );
-      }
+			// BD-Proposal-Details
+			if (data.bd_proposal_detail) {
+				savedEntities.bd_proposal_detail =
+					await strapi.entityService.create(
+						'api::bd-proposal-detail.bd-proposal-detail',
+						{
+							data: data.bd_proposal_detail,
+						}
+					);
+			}
 
-      // BD-Contact-Information
-      if (data.bd_contact_information) {
-        savedEntities.bd_contact_information =
-          await strapi.entityService.create(
-            "api::bd-contact-information.bd-contact-information",
-            {
-              data: data.bd_contact_information,
-            }
-          );
-      }
+			// BD-Proposal-Ownership
+			if (data.bd_proposal_ownership) {
+				savedEntities.bd_proposal_ownership =
+					await strapi.entityService.create(
+						'api::bd-proposal-ownership.bd-proposal-ownership',
+						{
+							data: data.bd_proposal_ownership,
+						}
+					);
+			}
 
-      //BD-Further-Information
-      if (data.bd_further_information) {
-        const furtherInfoData = { ...data.bd_further_information };
-        if (furtherInfoData.proposal_links) {
-          savedEntities.bd_further_information =
-            await strapi.entityService.create(
-              "api::bd-further-information.bd-further-information",
-              {
-                data: furtherInfoData,
-              }
-            );
-        }
-      }
+			// BD-Contact-Information
+			if (data.bd_contact_information) {
+				savedEntities.bd_contact_information =
+					await strapi.entityService.create(
+						'api::bd-contact-information.bd-contact-information',
+						{
+							data: data.bd_contact_information,
+						}
+					);
+			}
 
-      const mainEntryData = {
-        privacy_policy: data.privacy_policy,
-        intersect_named_administrator:
-          data.intersect_named_administrator || false,
-        old_ver: data.old_ver,
-        creator: user.id,
-        bd_psapb: savedEntities.bd_psapb?.id || null,
-        bd_costing: savedEntities.bd_costing?.id || null,
-        bd_proposal_detail: savedEntities.bd_proposal_detail?.id || null,
-        bd_proposal_ownership: savedEntities.bd_proposal_ownership?.id || null,
-        bd_contact_information:
-          savedEntities.bd_contact_information?.id || null,
-        bd_further_information:
-          savedEntities.bd_further_information?.id || null,
-      };
+			//BD-Further-Information
+			if (data.bd_further_information) {
+				const furtherInfoData = { ...data.bd_further_information };
+				if (furtherInfoData.proposal_links) {
+					savedEntities.bd_further_information =
+						await strapi.entityService.create(
+							'api::bd-further-information.bd-further-information',
+							{
+								data: furtherInfoData,
+							}
+						);
+				}
+			}
 
-      const createdEntry = await strapi.entityService.create("api::bd.bd", {
-        data: mainEntryData,
-        populate: {
-          bd_psapb: true,
-          bd_costing: true,
-          bd_proposal_detail: true,
-          bd_proposal_ownership: true,
-          bd_contact_information: true,
-          bd_further_information: {
-            populate: ["proposal_links"],
-          },
-          creator: true,
-        },
-      });
+			let latestVersionId = null;
 
-      if (data?.old_ver) {
-        await strapi.entityService.update("api::bd.bd", data.old_ver, {
-          data: { is_active: false },
-        });
-        const poll = await strapi.entityService.findMany(
-          "api::bd-poll.bd-poll",
-          { filters: { bd_proposal_id: data.old_ver } }
-        );
-        if (poll.length > 0) {
-          await strapi.entityService.update(
-            "api::bd-poll.bd-poll",
-            poll[0].id,
-            { data: { bd_proposal_id: createdEntry.id.toString() } }
-          );
-        }
-        const comments = await strapi.entityService.findMany(
-          "api::comment.comment",
-          { filters: { bd_proposal_id: data.old_ver } }
-        );
-        if (comments.length > 0) {
-          for (const comment of comments) {
-            await strapi.entityService.update(
-              "api::comment.comment",
-              comment.id,
-              { data: { bd_proposal_id: createdEntry.id.toString() } }
-            );
-          }
-        }
-      } else {
-        await strapi.entityService.create("api::bd-poll.bd-poll", {
-          data: { bd_proposal_id: createdEntry.id.toString() },
-        });
-      }
+			if (data?.old_ver) {
+				// find me this bd and check if is_active, if no - find bd where that old_ver is placed as old_ver in some other bd, and if that is not active, find a bd where parent id is old_ver and recursively get  to the version where that is the latest version of that bd
+				latestVersionId = await getLastOldVersion(data?.old_ver);
+			}
 
-      return createdEntry;
-    } catch (error) {
-      console.log(error);
-      strapi.log.error("FULL CREATE ERROR:", {
-        message: error.message,
-        stack: error.stack,
-        request: ctx.request.body,
-      });
-      return ctx.internalServerError(
-        "There was an error when saving the entire BD entry"
-      );
-    }
-  },
-  async bdProposalVersions(ctx) {
-    const { id } = ctx.params;
-    if (!id) {
-      return ctx.badRequest("ID is required");
-    }
+			const mainEntryData = {
+				privacy_policy: data.privacy_policy,
+				intersect_named_administrator:
+					data.intersect_named_administrator || false,
+				old_ver: latestVersionId || null,
+				creator: user.id,
+				bd_psapb: savedEntities.bd_psapb?.id || null,
+				bd_costing: savedEntities.bd_costing?.id || null,
+				bd_proposal_detail:
+					savedEntities.bd_proposal_detail?.id || null,
+				bd_proposal_ownership:
+					savedEntities.bd_proposal_ownership?.id || null,
+				bd_contact_information:
+					savedEntities.bd_contact_information?.id || null,
+				bd_further_information:
+					savedEntities.bd_further_information?.id || null,
+			};
 
-    const populate = [
-      "creator",
-      "old_ver",
-      "bd_costing.preferred_currency",
-      "bd_proposal_detail.contract_type_name",
-      "bd_further_information.proposal_links",
-      "bd_psapb.type_name",
-      "bd_psapb.roadmap_name",
-      "bd_psapb.committee_name",
-      "bd_proposal_ownership.be_country",
-    ];
-    let current = await strapi.entityService.findOne("api::bd.bd", id, {
-      populate: populate,
-    });
-    if (!current) {
-      return ctx.notFound("Resource not found");
-    }
+			const createdEntry = await strapi.entityService.create(
+				'api::bd.bd',
+				{
+					data: mainEntryData,
+					populate: {
+						bd_psapb: true,
+						bd_costing: true,
+						bd_proposal_detail: true,
+						bd_proposal_ownership: true,
+						bd_contact_information: true,
+						bd_further_information: {
+							populate: ['proposal_links'],
+						},
+						creator: true,
+					},
+				}
+			);
 
-    const versions = [current];
+			if (latestVersionId) {
+				await strapi.entityService.update(
+					'api::bd.bd',
+					latestVersionId,
+					{
+						data: { is_active: false },
+					}
+				);
+				const poll = await strapi.entityService.findMany(
+					'api::bd-poll.bd-poll',
+					{ filters: { bd_proposal_id: latestVersionId } }
+				);
+				if (poll.length > 0) {
+					await strapi.entityService.update(
+						'api::bd-poll.bd-poll',
+						poll[0].id,
+						{ data: { bd_proposal_id: createdEntry.id.toString() } }
+					);
+				}
+				const comments = await strapi.entityService.findMany(
+					'api::comment.comment',
+					{ filters: { bd_proposal_id: latestVersionId } }
+				);
+				if (comments.length > 0) {
+					for (const comment of comments) {
+						await strapi.entityService.update(
+							'api::comment.comment',
+							comment.id,
+							{
+								data: {
+									bd_proposal_id: createdEntry.id.toString(),
+								},
+							}
+						);
+					}
+				}
+			} else {
+				await strapi.entityService.create('api::bd-poll.bd-poll', {
+					data: { bd_proposal_id: createdEntry.id.toString() },
+				});
+			}
 
-    while (current && current.old_ver) {
-      const previous = await strapi.entityService.findOne(
-        "api::bd.bd",
-        current?.old_ver?.id,
-        {
-          populate: populate,
-        }
-      );
+			return createdEntry;
+		} catch (error) {
+			console.log(error);
+			strapi.log.error('FULL CREATE ERROR:', {
+				message: error.message,
+				stack: error.stack,
+				request: ctx.request.body,
+			});
+			return ctx.internalServerError(
+				'There was an error when saving the entire BD entry'
+			);
+		}
+	},
+	async bdProposalVersions(ctx) {
+		const { id } = ctx.params;
+		if (!id) {
+			return ctx.badRequest('ID is required');
+		}
 
-      if (!previous) break;
-      versions.push(previous);
-      current = previous;
-    }
+		const populate = [
+			'creator',
+			'old_ver',
+			'bd_costing.preferred_currency',
+			'bd_proposal_detail.contract_type_name',
+			'bd_further_information.proposal_links',
+			'bd_psapb.type_name',
+			'bd_psapb.roadmap_name',
+			'bd_psapb.committee_name',
+			'bd_proposal_ownership.be_country',
+		];
+		let current = await strapi.entityService.findOne('api::bd.bd', id, {
+			populate: populate,
+		});
+		if (!current) {
+			return ctx.notFound('Resource not found');
+		}
 
-    return this.transformResponse(versions);
-  },
+		const versions = [current];
 
-  async findOne(ctx) {
-    const user = ctx.state.user;
-    const { id } = ctx.params;
-    const query = ctx.request.query;
+		while (current && current.old_ver) {
+			const previous = await strapi.entityService.findOne(
+				'api::bd.bd',
+				current?.old_ver?.id,
+				{
+					populate: populate,
+				}
+			);
 
-    const entity = await strapi.entityService.findOne("api::bd.bd", id, query);
+			if (!previous) break;
+			versions.push(previous);
+			current = previous;
+		}
 
-    const entitiCreator = await strapi.entityService.findOne("api::bd.bd", id, {
-      populate: ["creator"],
-    });
+		return this.transformResponse(versions);
+	},
 
-    if (
-      entity?.bd_contact_information &&
-      user?.id !== entitiCreator?.creator?.id
-    ) {
-      delete entity.bd_contact_information;
-    }
+	async findOne(ctx) {
+		const user = ctx.state.user;
+		const { id } = ctx.params;
+		const query = ctx.request.query;
 
-    if (entity?.creator) {
-      delete entity?.creator?.username;
-      delete entity?.creator?.email;
-      delete entity?.creator?.provider;
-      delete entity?.creator?.blocked;
-      delete entity?.creator?.createdAt;
-      delete entity?.creator?.updatedAt;
-      delete entity?.creator?.is_validated;
-      delete entity?.creator?.password;
-      delete entity?.creator?.confirmed;
-    }
+		const entity = await strapi.entityService.findOne(
+			'api::bd.bd',
+			id,
+			query
+		);
 
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-    return this.transformResponse(sanitizedEntity);
-  },
+		const entitiCreator = await strapi.entityService.findOne(
+			'api::bd.bd',
+			id,
+			{
+				populate: ['creator'],
+			}
+		);
 
-  async find(ctx) {
-    const query = ctx.request.query;
+		if (
+			entity?.bd_contact_information &&
+			user?.id !== entitiCreator?.creator?.id
+		) {
+			delete entity.bd_contact_information;
+		}
 
-    const { results, pagination } = await strapi.service("api::bd.bd").find({
-      ...query,
-    });
+		if (entity?.creator) {
+			delete entity?.creator?.username;
+			delete entity?.creator?.email;
+			delete entity?.creator?.provider;
+			delete entity?.creator?.blocked;
+			delete entity?.creator?.createdAt;
+			delete entity?.creator?.updatedAt;
+			delete entity?.creator?.is_validated;
+			delete entity?.creator?.password;
+			delete entity?.creator?.confirmed;
+		}
 
-    const sanitizedResults = results.map((entity) => {
-      if (entity?.bd_contact_information) {
-        delete entity.bd_contact_information;
-      }
+		const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+		return this.transformResponse(sanitizedEntity);
+	},
 
-      if (entity?.creator) {
-        delete entity?.creator?.username;
-        delete entity?.creator?.email;
-        delete entity?.creator?.provider;
-        delete entity?.creator?.blocked;
-        delete entity?.creator?.createdAt;
-        delete entity?.creator?.updatedAt;
-        delete entity?.creator?.is_validated;
-        delete entity?.creator?.password;
-        delete entity?.creator?.confirmed;
-      }
-      return entity;
-    });
+	async find(ctx) {
+		const query = ctx.request.query;
 
-    const sanitizedOutput = await this.sanitizeOutput(sanitizedResults, ctx);
-    return this.transformResponse(sanitizedOutput, { pagination });
-  },
+		const { results, pagination } = await strapi
+			.service('api::bd.bd')
+			.find({
+				...query,
+			});
+
+		const sanitizedResults = results.map((entity) => {
+			if (entity?.bd_contact_information) {
+				delete entity.bd_contact_information;
+			}
+
+			if (entity?.creator) {
+				delete entity?.creator?.username;
+				delete entity?.creator?.email;
+				delete entity?.creator?.provider;
+				delete entity?.creator?.blocked;
+				delete entity?.creator?.createdAt;
+				delete entity?.creator?.updatedAt;
+				delete entity?.creator?.is_validated;
+				delete entity?.creator?.password;
+				delete entity?.creator?.confirmed;
+			}
+			return entity;
+		});
+
+		const sanitizedOutput = await this.sanitizeOutput(
+			sanitizedResults,
+			ctx
+		);
+		return this.transformResponse(sanitizedOutput, { pagination });
+	},
 }));
