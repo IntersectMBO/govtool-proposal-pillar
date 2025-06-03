2dcd5d17dad628a705d05742899bab0c54e3b059
Milos Mihajlovic <mihajlovicmilos16@gmail.com>
2025-06-03 09:48:11 +0200

    chore: Update changelog for version 0.7.0-beta-34, fix data-testIds, and adjust link validation in LinkManager

--- pdf-ui/src/components/CreationGoveranceAction/LinkManager.jsx
+++ pdf-ui/src/components/CreationGoveranceAction/LinkManager.jsx
@@ -25,12 +25,14 @@ const LinkManager = ({
             ...proposalData,
             proposal_links: newLinks,
         });
+
         if (field === 'prop_link' && value === '') {
             return setLinksErrors((prev) => {
                 const { [index]: removed, ...rest } = prev;
                 return rest;
             });
         }
+
         if (field === 'prop_link') {
             let urlError = '';
             if (value.length > 2048) {
@@ -46,15 +48,26 @@ const LinkManager = ({
                 },
             }));
         }
+
         if (field === 'prop_link_text') {
             let textError = '';
             if (value.length > 255) {
                 textError = 'Text must be 255 characters or less';
             }
             setLinksErrors((prev) => {
-                if (value === '' || value < 255) {
-                    const { [index]: removed, ...rest } = prev;
-                    return rest;
+                if (value === '') {
+                    const currentErrors = prev[index] || {};
+                    const { text, ...otherErrors } = currentErrors;
+
+                    if (Object.keys(otherErrors).length === 0) {
+                        const { [index]: removed, ...rest } = prev;
+                        return rest;
+                    } else {
+                        return {
+                            ...prev,
+                            [index]: otherErrors,
+                        };
+                    }
                 }
                 return {
                     ...prev,
