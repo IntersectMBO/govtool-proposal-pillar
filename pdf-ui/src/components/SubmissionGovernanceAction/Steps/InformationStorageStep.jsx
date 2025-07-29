a046b3b95d16db36f28fdcda59eb7bf8464f170c
Milos Mihajlovic <mihajlovicmilos16@gmail.com>
2025-07-29 08:17:05 +0200

    feat: Enhance URL handling for IPFS links in InformationStorageStep and utils

--- pdf-ui/src/components/SubmissionGovernanceAction/Steps/InformationStorageStep.jsx
+++ pdf-ui/src/components/SubmissionGovernanceAction/Steps/InformationStorageStep.jsx
@@ -31,7 +31,6 @@ import { IconExternalLink } from '@intersect.mbo/intersectmbo.org-icons-set';
 import { useTheme } from '@emotion/react';
 
 const InformationStorageStep = ({ proposal, handleCloseSubmissionDialog }) => {
-   
     const theme = useTheme();
     const navigate = useNavigate();
     const { walletAPI, validateMetadata } = useAppContext();
@@ -112,12 +111,17 @@ const InformationStorageStep = ({ proposal, handleCloseSubmissionDialog }) => {
         const hash = await walletAPI.createHash(jsonLd);
         setHashData(hash);
     };
-    const proposalGATypeId= proposal?.attributes?.content?.attributes.gov_action_type_id;
+    const proposalGATypeId =
+        proposal?.attributes?.content?.attributes.gov_action_type_id;
     const handleGASubmission = async () => {
         try {
+            let url = fileURL;
             setCheckingDataModal(true);
+            if (fileURL.startsWith('ipfs://')) {
+                url = `https://ipfs.io/ipfs/${fileURL.replace('ipfs://', '')}`;
+            }
             const response = await validateMetadata({
-                url: fileURL,
+                url: url,
                 hash: hashData,
                 standard: 'CIP108',
             });
@@ -135,43 +139,52 @@ const InformationStorageStep = ({ proposal, handleCloseSubmissionDialog }) => {
                         await walletAPI.buildTreasuryGovernanceAction({
                             hash: hashData,
                             url: fileURL,
-                            withdrawals: getWithdrawalsArray()
+                            withdrawals: getWithdrawalsArray(),
                         });
-                }
-                else if (parseInt(proposalGATypeId) === 3)
-                {
-                    const constitUrl = proposal?.attributes?.content?.attributes.proposal_constitution_content.data.attributes.prop_constitution_url;   
+                } else if (parseInt(proposalGATypeId) === 3) {
+                    const constitUrl =
+                        proposal?.attributes?.content?.attributes
+                            .proposal_constitution_content.data.attributes
+                            .prop_constitution_url;
                     const constiUrlHash = await getHashFromUrl(constitUrl);
                     govActionBuilder =
-                    await walletAPI.buildNewConstitutionGovernanceAction({
-                        hash: hashData,
-                        url: fileURL,
-                        constitutionUrl: constitUrl,
-                        constitutionHash: constiUrlHash
-                    //prevGovernanceActionHash: string;
-                    //prevGovernanceActionIndex: number;
-                    //scriptHash: string;
-                    });
-                }
-                else if(parseInt(proposalGATypeId) === 4)
-                { ///Motion of No Confidence
+                        await walletAPI.buildNewConstitutionGovernanceAction({
+                            hash: hashData,
+                            url: fileURL,
+                            constitutionUrl: constitUrl,
+                            constitutionHash: constiUrlHash,
+                            //prevGovernanceActionHash: string;
+                            //prevGovernanceActionIndex: number;
+                            //scriptHash: string;
+                        });
+                } else if (parseInt(proposalGATypeId) === 4) {
+                    ///Motion of No Confidence
                     govActionBuilder =
                         await walletAPI.buildNoConfidenceGovernanceAction({
                             hash: hashData,
                             url: fileURL,
                         });
-                }
-                else if(parseInt(proposalGATypeId) === 6)
-                { ///Hard Fork Initiation
+                } else if (parseInt(proposalGATypeId) === 6) {
+                    ///Hard Fork Initiation
                     govActionBuilder =
-                        await walletAPI.buildHardForkInitiationGovernanceActions({
-                            prevGovernanceActionHash: proposal?.attributes?.content?.attributes?.proposal_hard_fork_content.previous_ga_hash,
-                            prevGovernanceActionIndex: proposal?.attributes?.content?.attributes?.proposal_hard_fork_content.previous_ga_id,
-                            major: proposal?.attributes?.content?.attributes?.proposal_hard_fork_content.major,
-                            minor: proposal?.attributes?.content?.attributes?.proposal_hard_fork_content.minor,
-                            hash: hashData,
-                            url: fileURL,
-                        });
+                        await walletAPI.buildHardForkInitiationGovernanceActions(
+                            {
+                                prevGovernanceActionHash:
+                                    proposal?.attributes?.content?.attributes
+                                        ?.proposal_hard_fork_content
+                                        .previous_ga_hash,
+                                prevGovernanceActionIndex:
+                                    proposal?.attributes?.content?.attributes
+                                        ?.proposal_hard_fork_content
+                                        .previous_ga_id,
+                                major: proposal?.attributes?.content?.attributes
+                                    ?.proposal_hard_fork_content.major,
+                                minor: proposal?.attributes?.content?.attributes
+                                    ?.proposal_hard_fork_content.minor,
+                                hash: hashData,
+                                url: fileURL,
+                            }
+                        );
                 }
 
                 if (govActionBuilder) {
@@ -212,12 +225,17 @@ const InformationStorageStep = ({ proposal, handleCloseSubmissionDialog }) => {
 
     const getWithdrawalsArray = () => {
         let withdrawalsArray = [];
-        let x = proposal?.attributes?.content?.attributes?.proposal_withdrawals.forEach((withdrawal) =>
-        {
-            withdrawalsArray.push({receivingAddress:withdrawal.prop_receiving_address,amount:withdrawal.prop_amount.toString()})
-        });
+        let x =
+            proposal?.attributes?.content?.attributes?.proposal_withdrawals.forEach(
+                (withdrawal) => {
+                    withdrawalsArray.push({
+                        receivingAddress: withdrawal.prop_receiving_address,
+                        amount: withdrawal.prop_amount.toString(),
+                    });
+                }
+            );
         return withdrawalsArray;
-    }
+    };
 
     const handleDownloadJsonLD = () => {
         const blob = new Blob([JSON.stringify(jsonLdData, null, 2)], {
@@ -239,7 +257,9 @@ const InformationStorageStep = ({ proposal, handleCloseSubmissionDialog }) => {
                 throw new Error('url is not defined or null');
             }
             // Fetch the data from the URL
-            const response = (await fetch(url,{'User-Agent': "govtool-agent"}));
+            const response = await fetch(url, {
+                'User-Agent': 'govtool-agent',
+            });
             // Check if the response is successful
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
